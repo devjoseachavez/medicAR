80dd19648fe8ab1bb5d706431aef495a2967e02a
unknown <devjoseachavez>
2026-03-11 21:32:24 -0300

    Subida de proyecto

---
+++ src/main/java/ar/com/chavezdrive/medicar/MedicarApplication.java
@@ -0,0 +1,13 @@
+package ar.com.chavezdrive.medicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/chavezdrive/medicar/MedicarApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.chavezdrive.medicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
